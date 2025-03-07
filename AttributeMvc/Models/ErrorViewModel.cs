739c98d8fc43c053cc48cff832a28a80746ca2de
Nart Candan <83220508+kharpadi@users.noreply.github.com>
2025-03-07 23:18:46 +0300

    Delete AttributeMvc directory

--- AttributeMvc/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace AttributeMvc.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
